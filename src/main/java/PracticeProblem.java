124a949b824e0c1d0e4955ff04931dd8b5f632f8
doriangoobie <dorian.goobie@student.tdsb.on.ca>
2026-02-17 15:35:22 +0000

    submit assignment

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,49 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		
 	}
 
-	
+	public static int sum(int x, int y) {
+		int result = x + y;
+		return result;
+	}
 
+	public static int difference(int a, int b) {
+		int result = a - b;
+		return result;
+	}
+
+	public static double product(double c, double d) {
+		double result = c * d;
+		return result;
+	}
+
+ 	public static String removeFirst(String e) {
+		String result = e.substring(1);
+		return result;
+	}
+
+	public static int combinedLength(String f, String g) {
+		int result = f.length() + g.length();
+		return result;
+	}
+	public static boolean isEven(int h) {
+		Boolean result = h % 2 == 0;
+		return result;
+	}
+	public static boolean isOdd(int i) {
+		boolean result = i % 2 != 0;
+		return result;
+	}
+	public static boolean isPositive(int j) {
+		boolean result = j > 0;
+		return result;
+	}
+
+	public static boolean isNegative(int k) {
+		boolean result = k < 0;
+		return result;
+	}
+	
 }
